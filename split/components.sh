184b88d0fbd87ae273942b225c6a8e06b8ffb026
dreamsbond <info@dreamsbond.com>
2020-05-31 17:18:51 +0800

    update.

--- split/components.sh
+++ split/components.sh
@@ -3,10 +3,10 @@
 BRANCHES='master develop'
 
 COMPONENTS=(
-    'components/Application/:git@github.com:niftycorner/limoncello-php-package-azure-ad.git'
+    'components/AzureAd/:git@github.com:niftycorner/limoncello-php-package-azure-ad.git'
     'components/Contracts/:git@github.com:niftycorner/limoncello-php-package-contracts.git'
     'components/Data/:git@github.com:niftycorner/limoncello-php-package-data.git'
     'components/Flute/:git@github.com:niftycorner/limoncello-php-package-flute.git'
-    'components/Passport/:git@github.com:niftycorner/limoncello-php-package-spreadsheet.git'
+    'components/Spreadsheet/:git@github.com:niftycorner/limoncello-php-package-spreadsheet.git'
     'components/Templates/:git@github.com:niftycorner/limoncello-php-package-templates.git'
 )
